706b8ea529cc0594816a264908b75b1528aac183
mattyc246 <mattyc246@gmail.com>
2018-09-13 15:56:22 +0800

    1. Bootstrap Added 2. Homepage Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
+  root 'home#index'
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
