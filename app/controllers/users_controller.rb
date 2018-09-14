a166f12f8b91143b82aad1973843ebbc4f3e2f53
mattyc246 <mattyc246@gmail.com>
2018-09-14 11:52:53 +0800

    1. Sign Up & Sign In All Working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
 
 	def create
-		@user = User.create(
-			params[:first_name],
-			params[:last_name],
-			params[:contact_number],
-			params[:birthday],
-			params[:email],
-			params[:password]
-			)
+		@user = User.create(user_params)
+		redirect_to '/'
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :contact_number, :date_of_birth, :email, :password)
 	end
 
 end
