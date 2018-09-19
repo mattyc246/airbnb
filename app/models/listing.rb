342c728500326332769a0331fe6d5456932159c8
mattyc246 <mattyc246@gmail.com>
2018-09-19 15:37:51 +0800

    1. Added validations between user & listing 2. Minor interface changes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,7 @@
 class Listing < ApplicationRecord
 
+  belongs_to :user
+
 	def self.for_select
 		{
                'Apartment' => ['Apartment','Condominium','Casa Particular','Loft','Serviced Apartment'],
