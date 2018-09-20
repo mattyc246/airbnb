ac24e2a01050743cc29f41e0449566635f88967f
mattyc246 <mattyc246@gmail.com>
2018-09-20 11:54:10 +0800

    1. Added Show Routes for User

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "clearance/sessions", only: [:create]
 
-  resources :users, only: [:create, :edit] do
+  resources :users, only: [:create, :edit, :show] do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
