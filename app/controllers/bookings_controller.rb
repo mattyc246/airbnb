5717f082adc999ebba888fdc5749dd39517047b6
mattyc246 <mattyc246@gmail.com>
2018-09-25 11:04:47 +0800

    1. Ability to add booking, no validation yet

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,15 +8,26 @@ class BookingsController < ActionController::Base
 
 	def create
 
-		byebug
+		@booking = Booking.new(booking_params)
 
+		if @booking.save
+
+			flash[:notice] = "Booking Successfully Made! Please make immediate payment to secure booking!"
+			redirect_to "/listings/#{params[:listing_id]}"
+
+		else
+
+			flash[:notice] = "Unable to make booking, dates already taken!"
+			redirect_to "/listings/#{params[:listing_id]}"
+
+		end
 	end
 
 	private
 
 	def booking_params
 
-		params.permit(:check_in, :check_out, :listing_id)
+		params.permit(:check_in, :check_out, :listing_id, :user_id)
 		
 
 	end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,16 +2,6 @@ class Booking < ApplicationRecord
 
 	belongs_to :user
 	belongs_to :listing
-	validate :check_dates
 
-	def check_dates
-
-		current_bookings = Booking.where(listing_id: self.listing.id)
-
-
-
-		self.check_in
-
-	end
 
 end
