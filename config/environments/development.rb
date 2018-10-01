56e85660796cbdf73bec7289905cc251e3af47fc
mattyc246 <mattyc246@gmail.com>
2018-10-01 14:54:21 +0800

    1. Force compile assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,6 +54,8 @@ Rails.application.configure do
 
   config.assets.digest = true
 
+  config.assets.compile = true
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
